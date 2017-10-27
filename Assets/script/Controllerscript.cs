f91fab4a7b898e7c5d466c844d5c47c038aec3ea
LAPTOP-CAA5F6OF\harut <haruto.ikashumai.626@gmail.com>
2017-10-27 18:52:35 +0900

    first

---
+++ Assets/script/Controllerscript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controllerscript : MonoBehaviour {
+	public Camera raycamera;
+	public int distance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Shot ();
+	}
+	void Shot(){
+		Vector3 center = new Vector3 (Screen.width / 2, Screen.height / 2, 0);
+	Ray ray = raycamera.ScreenPointToRay (center);
+		RaycastHit hitinfo;
+		if (Physics.Raycast (ray, out hitinfo, distance)) {
+//			Debug.Log ("hit");
+			Debug.DrawLine (ray.origin, hitinfo.point, Color.red);}
+	}
+}
---
+++ Assets/script/enemyscript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyscript : MonoBehaviour {
+	public int distance;
+//	public GameObject hed;
+	public GameObject gforward;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		finding (0,1);
+//		finding (1,0);
+//		finding (-1, 0);
+//		finding (0, 0);
+//		finding(0.5f,0);
+//		finding(-0.5f,0);
+//		finding(0.25f,0);
+//		finding(0,0);
+		finding(1,1);
+	}
+	void finding(float right,float up ){
+		Ray ray = new Ray (this.transform.position,this.transform.forward);
+//		Vector3 forward = gforward.transform.position - this.transform.position;
+//		Ray ray = new Ray(this.transform.position,forward );
+//		RaycastHit hitinfo;c
+		Debug.DrawLine (ray.origin, ray.direction, Color.red);
+//		if (Physics.Raycast (ray, out hitinfo, distance)) {
+//
+//			Debug.DrawLine (ray.origin, hitinfo.point, Color.red);}
+	}
+}
